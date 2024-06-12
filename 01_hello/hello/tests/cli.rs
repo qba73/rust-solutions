0c6cca252d4e6f3be4d0171a68d016dc06a07a83
Jakub Jarosz <jakub@jarosz.dev>
2024-06-12 09:46:09 +0100

    Add dependencies to tests

--- 01_hello/hello/tests/cli.rs
+++ 01_hello/hello/tests/cli.rs
@@ -1,8 +1,9 @@
 use std::process::Command;
 
+use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
+
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
-    assert!(res.is_ok());
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
 }
