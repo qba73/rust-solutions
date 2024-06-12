95dcad51653f1cb85a86c395b3cc6a67f44dea7f
Jakub Jarosz <jakub@jarosz.dev>
2024-06-12 09:57:52 +0100

    Add test to assert exact output

--- 01_hello/hello/tests/cli.rs
+++ 01_hello/hello/tests/cli.rs
@@ -1,9 +1,17 @@
-use std::process::Command;
-
 use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
+use std::process::Command;
 
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
     cmd.assert().success();
+}
+
+#[test]
+fn runs_with_out() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    let output = cmd.output().expect("fail");
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, "Hello, world!\n");
 }
